2ce5bc91b299ce5d776dec9ad9b9e644fd1011fb
josh-huskisson <70178679+josh-huskisson@users.noreply.github.com>
2022-09-21 11:29:17 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,14 +8,31 @@ using std::string;
 int main(){
     const string phrase = "The quick brown fox jumps over the lazy dog";
     string input;
+    int bestScore;
+    int finalScore;
 
     do{
         cout << "Type the following phrase and then press return:\n" 
             << phrase << endl;
 
         getline(cin,input);
+        
+        //TODO: Show results here        
+        
+        bestScore = 43;
+        finalScore = 43;
+        
+        finalScore += (input.length() - 43) * 2;
+        
+        for(int i = 0; i < phrase.length(); i++){
+            if(phrase[i] != input[i]){
+                finalScore++;   
+            }
+        }
+        
+        finalScore = bestScore / finalScore * 100;
+        cout << "Results: " << finalScore << "% accurate" << endl;
 
-        //TODO: Show results here
 
         do{
             cout << "Try again? (yes/no)\n";
