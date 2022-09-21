b628ea74c13105739f2f18f34ad0aab938317894
Dylan Hall <dhall1@mail.csuchico.edu>
2022-09-21 11:32:31 -0700

    Added features from issue #1

--- main.cpp
+++ main.cpp
@@ -10,12 +10,22 @@ int main(){
     string input;
 
     do{
-        cout << "Type the following phrase and then press return:\n" 
+        cout << "Type the following phrase and then press return:\n"
             << phrase << endl;
 
         getline(cin,input);
-
-        //TODO: Show results here
+        int count = 0;
+        //itterate trhough phrase and compare it to input
+        for(int i = 0; i < phrase.size(); i++){
+          if(phrase[i] == input[i]){
+            //add 100 per correct input to avoid decimals
+            count+=100;
+          }
+        }
+        //calculate percent
+        count = count / phrase.size();
+        //displays results
+        cout << "Results: " << count << "% accurate"<< endl;
 
         do{
             cout << "Try again? (yes/no)\n";
