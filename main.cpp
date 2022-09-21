31ef14f030c0a20d69f6d27cf3e60f7a2e6251c2
Amritvir Randhawa <arandhawa@mail.csuchico.edu>
2022-09-21 11:53:43 -0700

    added feature

--- main.cpp
+++ main.cpp
@@ -15,26 +15,15 @@ int main(){
     do{
         cout << "Type the following phrase and then press return:\n" 
             << phrase <<phrase2 << endl;
+            cout<<"Feature added"<<endl;
+
 
         getline(cin,input);
         
 
         //TODO: Show results here
-        if(phrase == phrase2)
-        cout << "100% Similarity!";
-    else{
-            if(phrase > phrase2)
-             vSz = phrase2.size();
-        else
-             vSz = phrase.size();
-
-        for(int i = 0; i != (vSz);++i){
-            if(phrase[i] == phrase2[i])
-                ++i;}
-        }
-    cout << "Strings have " << vSz << " characters in common. " << phrase 
-            << " is " << phrase.size() << " characters long and " << phrase2 << " is " 
-            << phrase2.size() << " long." << endl;
+        cout<<"Feature added"<<endl;
+    
 
 
         do{
