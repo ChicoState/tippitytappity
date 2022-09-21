25b6f9c5da070ba28b1eebca3b92dbaee98cd00f
Aryan <mittalaryan99@gmail.com>
2022-09-21 11:35:17 -0700

    added correctness analysis, resolves #1

--- main.cpp
+++ main.cpp
@@ -17,6 +17,27 @@ int main(){
 
         //TODO: Show results here
 
+        int accuracyPoints = 0;
+        for (int i=0; i < input.length(); i++) {
+            if (input[i] == phrase[i]) {
+                accuracyPoints++;
+            }
+        }
+
+        int total = (accuracyPoints / phrase.length()) * 100;
+        cout << "Total: " << total << endl;
+
+        int missingCharsNum = phrase.length() - input.length();
+        cout << "missing chars: " << missingCharsNum << endl;
+        if (missingCharsNum < 0) {
+            missingCharsNum *= -1;
+        }
+
+        int missingPoints = missingCharsNum * 2;
+        total -= missingPoints;
+
+        cout << "Results: " << total << "\% accurate" << endl;
+
         do{
             cout << "Try again? (yes/no)\n";
             getline(cin,input);
