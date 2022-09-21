87547e27d26911f82976bb54677ea6172ea4b501
Amritvir Randhawa <arandhawa@mail.csuchico.edu>
2022-09-21 11:45:46 -0700

    new feature added

--- main.cpp
+++ main.cpp
@@ -8,14 +8,34 @@ using std::string;
 int main(){
     const string phrase = "The quick brown fox jumps over the lazy dog";
     string input;
+    const string phrase2 = "The quick brown fox jumps ov the lay dog";
+    string input;
+    int vSz;
 
     do{
         cout << "Type the following phrase and then press return:\n" 
-            << phrase << endl;
+            << phrase <<phrase2 << endl;
 
         getline(cin,input);
+        
 
         //TODO: Show results here
+        if(phrase == phrase2)
+        cout << "100% Similarity!";
+    else{
+            if(phrase > phrase2)
+             vSz = phrase2.size();
+        else
+             vSz = phrase.size();
+
+        for(int i = 0; i != (vSz);++i){
+            if(phrase[i] == phrase2[i])
+                ++i;}
+        }
+    cout << "Strings have " << vSz << " characters in common. " << phrase 
+            << " is " << phrase.size() << " characters long and " << phrase2 << " is " 
+            << phrase2.size() << " long." << endl;
+
 
         do{
             cout << "Try again? (yes/no)\n";
