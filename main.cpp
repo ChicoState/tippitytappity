0c52a5b308c1d4c1eca33c29ae72ab2ff781f46d
computertyler <1supercomputertyler@gmail.com>
2022-09-21 11:33:34 -0700

    Update main.cpp
    Attempts to compare strings and display a percentage of how accurate the string is. Hopefully it meets the criteria of being case sensitive and displays the right percentage but it has been a while since I've done string comparison and I'm rusty on how to do it.  I tried my best...

--- main.cpp
+++ main.cpp
@@ -16,6 +16,18 @@ int main(){
         getline(cin,input);
 
         //TODO: Show results here
+        int compare = strcmp(phrase,input);
+        if (compare == 0){
+            cout << "100%";
+        }
+        else{
+            if (phrase.length() == input.length()) {
+                cout << compare/44*100;
+            }
+            else {
+                cout << (compare/44*100) - (2* (phrase.length() - input.length())); //subtract 2 points for every extra or missing character
+            }
+        }
 
         do{
             cout << "Try again? (yes/no)\n";
