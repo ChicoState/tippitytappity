a9ce1c9c97b4675e06c96b19fb3e46ee2a25a6ad
William Lyons <92234942+wtlyons54@users.noreply.github.com>
2022-09-21 11:45:03 -0700

    Added correctness analysis

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 using std::cin;
 using std::cout;
@@ -17,10 +18,29 @@ int main(){
 
         //TODO: Show results here
 	
+	int n = phrase.length();
+	char p[n+1];
+	strcpy(p, phrase.c_str());
+
+	char inp[n+1];
+	strcpy(inp, input.c_str());
+	
+
+	double num_correct=0;
+	double percent;
+
 	if(input == phrase){
-		cout << "You typed the following phrase correctly" << endl;
-		return 0;
-	}
+		cout << "Results: 100% accurate" << endl;
+	}else{
+		for(int i=0; i < 43; i++){
+			if(inp[i] == p[i]){
+				num_correct++;
+			}
+		}
+		percent = num_correct/43 * 100;
+		cout << "Results: " << percent << "% accurate" << endl; 
+}
+			
 
         do{
             cout << "Try again? (yes/no)\n";
