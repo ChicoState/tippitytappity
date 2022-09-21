863d9bfcd6941b878bebdba98e048c250e9eb4c0
Elijah Willis <ejwillis@mail.csuchico.edu>
2022-09-21 02:27:52 -0700

    fixed issue #1

--- main.cpp
+++ main.cpp
@@ -9,6 +9,9 @@ int main(){
     const string phrase = "The quick brown fox jumps over the lazy dog";
     string input;
 
+    double score;
+    int result;
+
     do{
         cout << "Type the following phrase and then press return:\n" 
             << phrase << endl;
@@ -16,6 +19,22 @@ int main(){
         getline(cin,input);
 
         //TODO: Show results here
+    	int inputS = input.size(), phraseS = phrase.size();
+	score = phraseS;
+	cout << "Score orig: " << score << endl;
+	if (inputS != phraseS)
+		score -= 2;
+
+	for (int i = 0; i < phraseS; i++) {
+		if (i < inputS) {
+			if (input[i] != phrase[i])
+				score--;
+		}
+	}
+
+	cout << "Score end: " << score << endl;
+	result = (score / phraseS * 100.0);
+	cout << "Score: " << result << endl;
 
         do{
             cout << "Try again? (yes/no)\n";
