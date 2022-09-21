8000efe3210ec307a5b8ff70c04f0c516507f99a
DevangRaval1 <draval@mail.csuchico.edu>
2022-09-21 11:54:24 -0700

    Resolve Conflicts-closes #1, closes #2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstring>
 #include <string>
+#include <chrono>
 using std::cin;
 using std::cout;
 using std::endl;
@@ -10,14 +11,19 @@ using namespace std;
 int main(){
     const string phrase = "The quick brown fox jumps over the lazy dog";
     string input;
+    std::chrono::steady_clock::time_point started; 
+    std::chrono::steady_clock::time_point stopped;
+    long milliseconds = 0;
 
     do{
         cout << "Type the following phrase and then press return:\n" 
             << phrase << endl;
-
+        started = std::chrono::steady_clock::now();
         getline(cin,input);
-
+        stopped = std::chrono::steady_clock::now(); // stop timer
+        milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(stopped - started).count();
         //TODO: Show results here
+        cout << "You took " << milliseconds / 1000.0 << " seconds" << endl;
         //code
         if(phrase.compare(input) == 0){
             cout << "100%" << endl;
