2aadf35a0acc097b353140c1418eff06231099e3
DevangRaval1 <draval@mail.csuchico.edu>
2022-09-21 11:46:00 -0700

    Added percentages of mistake made

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <cstring>
 #include <string>
 using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
+using namespace std;
 
 int main(){
     const string phrase = "The quick brown fox jumps over the lazy dog";
@@ -14,8 +16,29 @@ int main(){
             << phrase << endl;
 
         getline(cin,input);
-        
+
         //TODO: Show results here
+        if(phrase.compare(input) == 0){
+            cout << "100%" << endl;
+        }
+        else{
+            char check[phrase.length()]={};
+            char phr[phrase.length()]={};
+            strcpy(check, input.c_str());
+            strcpy(phr, phrase.c_str());
+            int mistake = 0;
+            int per = 0;
+            
+            for(int i = 0; i < phrase.length(); i++){
+                
+                
+                if(check[i]!=phr[i]){
+                    mistake++;
+                }
+            }
+            per = (43-mistake)*100/phrase.length();
+            cout << per <<"%"<< endl;
+        }
 
         do{
             cout << "Try again? (yes/no)\n";
