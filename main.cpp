ec754cd35179bd55aff6640c998ba04809ae6c55
manicdepravity <andrewjh1986@outlook.com>
2022-09-21 11:38:43 -0700

    added return score feature

--- main.cpp
+++ main.cpp
@@ -1,19 +1,36 @@
 #include <iostream>
+#include <vector>
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
+using std::vector;
 
 int main(){
     const string phrase = "The quick brown fox jumps over the lazy dog";
     string input;
 
     do{
-        cout << "Type the following phrase and then press return:\n" 
+        cout << "Type the following phrase and then press return:\n"
             << phrase << endl;
 
+        string ini = "The quick brown fox jumps over the lazy dog";
         getline(cin,input);
+
+        vector<string> ans(phrase.size(),input);
+        vector<string> ret(phrase.size(), phrase);
+        long long total = 0;
+        for(int i = 0; i < phrase.length(); i++) {
+            for (int j = 0; j < input.length(); j++) {
+
+                if (ans[i] == ret[j]) {
+                    total += 1;
+
+                }
+            }
+        }
+        cout << total/44 << endl;
 
         //TODO: Show results here
 
@@ -25,3 +42,4 @@ int main(){
 
     return 0;
 }
+
