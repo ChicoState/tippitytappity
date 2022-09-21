842e1b8d251939bc1ccf8a5908560f0c0ec5e69d
KadynCBR <kadyn_martinez@hotmail.com>
2022-09-21 11:26:09 -0700

    Correctness analysis added along with output. closes #1 Continuous integration added as well closes #2

--- main.cpp
+++ main.cpp
@@ -5,23 +5,31 @@ using std::cout;
 using std::endl;
 using std::string;
 
-int main(){
-    const string phrase = "The quick brown fox jumps over the lazy dog";
-    string input;
+int main() {
+  const string phrase = "The quick brown fox jumps over the lazy dog";
+  string input;
+  int result;
+  int demerit = 0;
 
-    do{
-        cout << "Type the following phrase and then press return:\n" 
-            << phrase << endl;
+  do {
+    cout << "Type the following phrase and then press return:\n" << phrase << endl;
 
-        getline(cin,input);
+    getline(cin, input);
 
-        //TODO: Show results here
+    for (int i = 0; i < phrase.length(); i++) {
+      if (i < input.length() && phrase[i] != input[i]) demerit++;
+      if (i >= input.length()) demerit++;
+    }
 
-        do{
-            cout << "Try again? (yes/no)\n";
-            getline(cin,input);
-        }while( input != "yes" && input != "no" );
-    }while( input == "yes" );
+    result = ((43 - demerit) / 43.0) * 100;
 
-    return 0;
+    cout << "Results: " << result << "% accurate" << endl;
+
+    do {
+      cout << "Try again? (yes/no)\n";
+      getline(cin, input);
+    } while (input != "yes" && input != "no");
+  } while (input == "yes");
+
+  return 0;
 }
